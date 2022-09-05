178643b3918cb72b5724e19a0c340914039a39cc
VanyaShestakov <vanka1108@gmail.com>
2022-09-05 18:12:21 +0300

    initial

---
+++ src/main/java/com/ivanshestakov/apiproviderservice/ApiProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ivanshestakov.apiproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProviderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProviderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanshestakov/apiproviderservice/ApiProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanshestakov.apiproviderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProviderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
